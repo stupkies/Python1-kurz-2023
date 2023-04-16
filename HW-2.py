2b60178ba9613d301283c5d3fad912525cbcfc88
stupkies <129364790+stupkies@users.noreply.github.com>
2023-04-16 14:52:44 +0200

    HW2-oprava

--- HW-2.py
+++ HW-2.py
@@ -11,28 +11,21 @@ sklad = {
 
 #Software se nejprve zeptá na kód součástky a poté na množství, které si zákazník chce koupit
 kod_soucastky=input("Zadej kod_soucastky")
-mnozstvi_na_skladu=int(input("Kolik kusů:"))
-#Pokud zadaný kód není ve slovníku, není součástka skladem.
-if kod_soucastky not in sklad:
-     print("Součástka není skladem")
-else:
+počet=int(input("Kolik kusů:"))
+#Pokud zadaný kód je ve slovníku, součástka je skladem.
+if kod_soucastky in sklad:
      print("Součástka je skladem")
-#Součástka na skladě je, ale je jí méně, než požaduje zákazník
-pozadovany_pocet=sklad[kod_soucastky]
-if pozadovany_pocet < mnozstvi_na_skladu:
-     print(f"Máme jenom {pozadovany_pocet} ks {kod_soucastky}")
-     #pokud se sníží požadovaný počet ks na 0 (protože se prodalo zákazníkovi) vymazat se skladu
-     del sklad[kod_soucastky]
+ #pokud chce víc než máme, dostane info o maximálním počtu
+     if kod_soucastky in sklad and sklad[kod_soucastky] < počet:
+          print(f'Bohužel této {kod_soucastky} máme jenom {sklad[kod_soucastky]} kusů') 
+     # odpočet na skladu
+     sklad[kod_soucastky] -= počet
+     # když je 0, tak vymaže
+     if sklad[kod_soucastky] ==0:
+          del sklad[kod_soucastky]
 else:
-     #součástek je dostatek, sníží se množství na skladu
-     print("Součástek máme dostatek")
-     sklad.pop(kod_soucastky)
+     print("Součástka není skladem")
+print(sklad)
 
-     
-
-
-  
  
-     
-
-
+ 
